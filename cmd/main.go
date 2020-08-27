1c410ddf045fd051aa984e6d9dde21a08c8eb710
Lars Hanssen <larsh.hanssen@gmail.com>
2020-08-27 16:51:55 +0200

    Test modules

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,19 @@
-package stocktracker
+package main
 
+import (
+	"fmt"
 
-func main(){
+	"github.com/piquette/finance-go/quote"
+)
 
+func main() {
+
+	q, err := quote.Get("AAPL")
+	if err != nil {
+		// Uh-oh.
+		panic(err)
+	}
+
+	// Success!
+	fmt.Println(q)
 }
